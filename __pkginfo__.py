e4a197cb794b3f50c5951b36bcef27c38c0fb284
Aurelien Campeas <aurelien.campeas@logilab.fr>
2009-04-06 14:12:45 +0200

    prepare 0.21.0

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -13,7 +13,7 @@ __docformat__ = "restructuredtext en"
 modname = 'yams'
 
 # release version
-numversion = (0, 20, 2)
+numversion = (0, 21, 0)
 version = '.'.join(str(num) for num in numversion)
 
 # license and copyright
