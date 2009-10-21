b4d65cfbd4d301ed4e3d803e150af065536fbc3d
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2009-10-21 09:02:00 +0200

    0.25

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -13,7 +13,7 @@ __docformat__ = "restructuredtext en"
 modname = 'yams'
 
 # release version
-numversion = (0, 24, 4)
+numversion = (0, 25, 0)
 version = '.'.join(str(num) for num in numversion)
 
 # license and copyright
