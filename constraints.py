ebe3994b467fce4bb4ed4bc2d57c2df400705173
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2010-09-23 16:04:32 +0200

    note and cleanup

--- constraints.py
+++ constraints.py
@@ -15,9 +15,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public License along
 # with yams. If not, see <http://www.gnu.org/licenses/>.
-"""Some common constraint classes.
-
-"""
+"""Some common constraint classes."""
 
 __docformat__ = "restructuredtext en"
 
