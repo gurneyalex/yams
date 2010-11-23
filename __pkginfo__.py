892ca1de2bf6bf80889db4481b3b98a2b3d97055
Pierre-Yves David <pierre-yves.david@logilab.fr>
2010-11-23 15:29:15 +0100

    [pkg] prepare 0.30.2 release

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -24,7 +24,7 @@ __docformat__ = "restructuredtext en"
 modname = 'yams'
 
 # release version
-numversion = (0, 30, 1)
+numversion = (0, 30, 2)
 version = '.'.join(str(num) for num in numversion)
 
 # license and copyright
