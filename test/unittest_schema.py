bc38aaa1a09cf7d71df0af788b1df21111f5465e
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2011-01-06 18:51:59 +0100

    add future import for python 2.5

--- test/unittest_schema.py
+++ test/unittest_schema.py
@@ -18,6 +18,8 @@
 # with yams. If not, see <http://www.gnu.org/licenses/>.
 """unit tests for module yams.schema classes"""
 
+from __future__ import with_statement
+
 from logilab.common.testlib import TestCase, unittest_main
 
 from copy import copy, deepcopy
