b09d9ffdff2eab20d9c4ce375c8d40c656bbc489
Julien Cristau <julien.cristau@logilab.fr>
2013-08-23 14:49:55 +0200

    Typo fix

--- __init__.py
+++ __init__.py
@@ -117,7 +117,7 @@ def register_base_type(name, parameters=(), check_function=None):
     from yams.schema import RelationDefinitionSchema
     from yams.constraints import BASE_CHECKERS, yes
     # Add the datatype to yams base types
-    assert name not in BASE_TYPES, '%s alreadt in BASE_TYPES %s' % (name, BASE_TYPES)
+    assert name not in BASE_TYPES, '%s already in BASE_TYPES %s' % (name, BASE_TYPES)
     BASE_TYPES.add(name)
     # Add the new datatype to the authorized types of RelationDefinitionSchema
     if not isinstance(parameters, dict):
