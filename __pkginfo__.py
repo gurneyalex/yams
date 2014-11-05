036bf2376785185aea77f3b6324ee78fa9fefe78
Julien Cristau <julien.cristau@logilab.fr>
2014-11-05 11:55:16 +0100

    [pkg] Prepare 0.40.0

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -24,7 +24,7 @@ __docformat__ = "restructuredtext en"
 modname = 'yams'
 
 # release version
-numversion = (0, 39, 1)
+numversion = (0, 40, 0)
 version = '.'.join(str(num) for num in numversion)
 
 # license and copyright
